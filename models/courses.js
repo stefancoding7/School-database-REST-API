26e6ec38ae237a8977a7985bb0f56d9308c59230
Stefan Caky <stefan.caky777@gmail.com>
2021-04-13 21:35:11 +0100

    desription
    change string to text

--- models/courses.js
+++ models/courses.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
       validate: {
         notNull: {
